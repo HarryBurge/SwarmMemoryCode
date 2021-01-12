35e696e902fe96af31c6a1958459ad906749d62b
Harry Burge <burgeharry@gmail.com>
2021-01-12 14:43:25 +0000

    .

--- Data/average_plot_multi.py
+++ Data/average_plot_multi.py
@@ -16,7 +16,7 @@ outer = np.nan_to_num(outer)
 #outer = np.nan_to_num(outer)
 
 # Indurvidual thing
-fig, axs = plt.subplots(2, sharex=True)
+fig, axs = plt.subplots(3, sharex=True)
 
 iteration= outer[:,0]
 
@@ -24,7 +24,7 @@ number_chans = (outer.shape[1]-2)//5
 
 for i in range(number_chans):
     axs[0].plot(iteration, gaussian_filter1d(outer[:,2+i*5], sigma=50))
-    #axs[1].plot(iteration, gaussian_filter1d(outer[:,1+i*5], sigma=50))
+    axs[1].plot(iteration, gaussian_filter1d(outer[:,1+i*5], sigma=50))
 
 
 omean = 0
@@ -38,21 +38,21 @@ for i in range(number_chans):
     omax+= outer[:,5+i*5] / number_chans
     omin+= outer[:,6+i*5] / number_chans
 
-line1 = axs[1].fill_between(iteration, gaussian_filter1d(omin, sigma=50), gaussian_filter1d(omax, sigma=50))
+line1 = axs[2].fill_between(iteration, gaussian_filter1d(omin, sigma=50), gaussian_filter1d(omax, sigma=50))
 line1.set_color('orange')
-line2 = axs[1].fill_between(iteration, gaussian_filter1d(omean+ostd, sigma=50), gaussian_filter1d(omean-ostd, sigma=50))
+line2 = axs[2].fill_between(iteration, gaussian_filter1d(omean+ostd, sigma=50), gaussian_filter1d(omean-ostd, sigma=50))
 line2.set_color('green')
-line3, = axs[1].plot(gaussian_filter1d(omean, sigma=50))
+line3, = axs[2].plot(gaussian_filter1d(omean, sigma=50))
 line3.set_color('purple')
 
 
 axs[0].set_ylabel('Number of Agents with duplicated data')
 axs[0].set_ylim(bottom=0)
 
-#axs[1].set_ylabel('Number of Agents')
-#axs[1].set_ylim(top=52, bottom=0)
+axs[1].set_ylabel('Number of Agents')
+axs[1].set_ylim(top=52, bottom=0)
 
-axs[1].set_ylabel('Distance from desired point')
-axs[1].set_xlabel('Iterations')
+axs[2].set_ylabel('Distance from desired point')
+axs[2].set_xlabel('Iterations')
 
 plt.show()
